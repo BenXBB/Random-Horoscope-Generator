c26f165548fee55d76f47f2734e94a704a911e9e
Ben <benb803@hotmail.co.uk>
2021-07-01 16:46:39 +0100

    Returns random message & MD corrected

--- README.md
+++ README.md
@@ -3,14 +3,13 @@
  ## Table of contents
 * [General info](#general-info)
 * [Technologies](#technologies)
-* [Setup](#setup)
 
 ## General info
-This project takes one input from the user and outputs a random string based on the users star sign  
+This project returns a random output string based on a star sign  
 <br>
 ## Technologies
 Project is created with:
 * VS code
 * Node.JS
-* Javascript ES7
+* Javascript ES6
 <br>	
--- phraseGenerator.js
+++ phraseGenerator.js
@@ -1,11 +1,25 @@
+/* TEMPLATE MESSAGE
 
+The random star sign is -------- and shares the plantery body of --------. You're feeling ------ from recent events. 
+This emotion may or may not seem the way you currently feel at this point in time but ultimately you're just ----------. 
+You should be absoutely ------ of the future.
 
+*/
 
 const starSigns = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const starPlanet = ["Mars", "Venus", "Mercury", "Moon", "Sun", "Mercury", "Venus", "Pluto", "Jupiter", "Saturn", "Uranus", "Neptune"];
 const feelingArr = ["hopeful", "greatful", "angry", "sadness", "enjoyment", "disgust", "relief", "contentment", "joy", "peace", "stressed", "anxious", "lonely", "lost", "desperate"];
 const verb = ["having fun", "finding yourself", "shaking off the rust", "waiting for a sign", "standing up for yourself", "living your best life", "mourning", "not accepting the past", "losing yourself"];
 const future = ["Weary", "hopeful", "anxious", "calm", "accepting", "open to new ideas", "active"]
 
-/* You're feeling ------ and -------- from recent events. These two emotions may or may not seem they go hand in hand
-but ultimately you're just ----------. You should be absoutely ------ of the future
-*/
+function randomHoroscope () {
+    let randomStarSign = Math.floor(Math.random() * starSigns.length);
+    let randomFeelingArr = Math.floor(Math.random() * feelingArr.length);
+    let randomVerb = Math.floor(Math.random() * verb.length);
+    let randomFuture = Math.floor(Math.random() * future.length);
+
+    return `The random star sign choosen is ${starSigns[randomStarSign]} and shares the plantery body of ${starPlanet[randomStarSign]}.\nYou're feeling ${feelingArr[randomFeelingArr]} from recent events. \nThis emotion may or may not seem the way you currently feel at this point in time but ultimately you're just ${verb[randomVerb]}. \nYou should be absoutely ${future[randomFuture]} of the future.`
+};
+
+
+console.log(randomHoroscope());
