1c100d16180565f966d334cf447611a19510d021
Ben <benb803@hotmail.co.uk>
2021-07-01 16:01:55 +0100

    added random arrays

--- phraseGenerator.js
+++ phraseGenerator.js
@@ -1,5 +1,11 @@
-console.log("Hello World");
 
-var hi = "Hello world";
 
-console.log(hi);
+
+const starSigns = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const feelingArr = ["hopeful", "greatful", "angry", "sadness", "enjoyment", "disgust", "relief", "contentment", "joy", "peace", "stressed", "anxious", "lonely", "lost", "desperate"];
+const verb = ["having fun", "finding yourself", "shaking off the rust", "waiting for a sign", "standing up for yourself", "living your best life", "mourning", "not accepting the past", "losing yourself"];
+const future = ["Weary", "hopeful", "anxious", "calm", "accepting", "open to new ideas", "active"]
+
+/* You're feeling ------ and -------- from recent events. These two emotions may or may not seem they go hand in hand
+but ultimately you're just ----------. You should be absoutely ------ of the future
+*/
